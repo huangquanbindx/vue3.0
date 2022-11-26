4e9f8498d7038fb652af15f7e7c73a87a401797e
huangquanbin <1348849173@qq.com>
2022-11-26 16:36:04 +0800

    提交着玩玩

--- src/views/aside/foodList.vue
+++ src/views/aside/foodList.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>{{ logs }}</div>
+  <div>{{ logs }}
+    <logbtn @run=ok() @running="no()"></logbtn>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, onMounted, reactive, toRefs, ref, Ref } from "vue";
+import logbtn from '../../components/btns/logbtn.vue';
 
 export default defineComponent({
   name: "foodList",
+  components:{logbtn},
   setup() {
     const logs = ref("one");
+    const ok=()=>{
+      console.log(1);
+    }
+    const no=()=>{
+      console.log(2);
+      
+    }
     return {
       logs,
+      ok,
+      no
     };
   },
 });
